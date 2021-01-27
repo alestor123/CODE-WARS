42af8b557d4d218e76d44ee402ff2734f3359aba
alestor123 <alestoraldous@gmail.com>
2021-01-27 16:34:14 +0530

    added cli code

--- cli.js
+++ cli.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
-
+var infos = require('./App');
+(async () => {
+var data = await infos()
+console.log(`Username : ${data.user}\nName : ${data.name}Email : ${data.email}Ip : ${data.ip}\nHost : ${data.host} \nCity : ${data.city} \nCountry : ${data.country}\nRegion : ${data.region}\nLocation : ${data.location}\nTime Zone : ${data.timezone}`)
+})();
+    
